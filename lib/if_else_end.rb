0bccb1932181b2e7be5f1dc9d0d75d1137d37ddb
ultimate-scope-5575 <a.bystranowska.takahashi@gmail.com>
2020-04-25 13:43:54 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,9 @@
 # Write your solution here
+current_time = Time.now
+current_seconds = current_time.to_i
+# if Time.now.to_i % 2 == 0
+if current_seconds % 2 == 0
+  puts "Even!"
+else
+  puts "Odd!"
+end
